6c27d7cfba5674a1841fcad4e21bb74a94ddcf25
Debdulal Baidya <ovibaidya72@gmail.com>
2023-04-17 22:21:18 +0600

    update Style

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -12,7 +12,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('assets/cropper.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/custom2.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/custom3.css') }}" rel="stylesheet">
 
     <title></title>
 </head>
@@ -121,7 +121,7 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('assets/cropper.min.js') }}"></script>
-    <script src="{{ asset('assets/custom2.js') }}"></script>
+    <script src="{{ asset('assets/custom3.js') }}"></script>
     <script>
         var bs_modal = $("#modal");
         var image = document.getElementById("image");
