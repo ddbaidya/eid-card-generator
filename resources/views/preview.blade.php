c2effe790e468ad67a802f80952b7753797bd17f
Debdulal Baidya <ovibaidya72@gmail.com>
2023-04-17 17:59:52 +0600

    Resolve CSS issue

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ asset('assets/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/custom1.css') }}" rel="stylesheet">
 
     <title>ঈদ মোবারক </title>
 </head>
@@ -29,7 +29,7 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('assets/custom.js') }}"></script>
+    <script src="{{ asset('assets/custom1.js') }}"></script>
 </body>
 
 </html>
